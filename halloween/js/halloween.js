28e77164c28a28e731bfaa85067581143ec085a7
aherlei <aherlei564@g.educaand.es>
2023-11-25 12:47:31 +0100

    halloween arreglos

--- halloween/js/halloween.js
+++ halloween/js/halloween.js
@@ -1,15 +1,29 @@
 const hands = document.getElementsByClassName('hands')
+const element = document.getElementById(element)
+
 
 function show(element) {
     document.getElementById(element).style.visibility = "visible"
 }
-function showTwoElments(element, element2) {
-    document.getElementById(element).style.visibility = "visible"
-    hands.style.visibility = "visible"
+
+/* MANOS A LA VEZ */
+function showTwoElments() {
+    for (let index = 0; index < hands.length; index++) {
+        hands[index].style.display = "inline"
+    }
+}
+
+function hiddenElments() {
+    for (let index = 0; index < hands.length; index++) {
+        hands[index].style.display = "none"
+    }
 }
 
 function hide(element) {
     document.getElementById(element).style.visibility = "hidden"
+}
+function none(element){
+    document.getElementById(element).classList.toggle("look");
 }
 
 function sound(element) {
