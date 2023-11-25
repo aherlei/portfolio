b1d68b07be7e671c3cc354b0d2aed0e422c28f20
aherlei <aherlei564@g.educaand.es>
2023-11-25 11:17:45 +0100

    pagina halloweeen

--- halloween/js/halloween.js
+++ halloween/js/halloween.js
@@ -1,12 +1,21 @@
-const hand = document.getElementById("door")
+const hands = document.getElementsByClassName('hands')
 
 function show(element) {
-    document.getElementById("hand").style.visibility = "visible"
+    document.getElementById(element).style.visibility = "visible"
 }
+function showTwoElments(element, element2) {
+    document.getElementById(element).style.visibility = "visible"
+    hands.style.visibility = "visible"
+}
+
 function hide(element) {
-    document.getElementById("hand").style.visibility = "hidden"
+    document.getElementById(element).style.visibility = "hidden"
 }
 
 function sound(element) {
-    document.getElementById("dooropen").play();
+    document.getElementById(element).play();
+}
+
+function silence(element) {
+    document.getElementById(element).pause();
 }
