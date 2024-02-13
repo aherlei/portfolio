d0320972830d5f2dabe19a7c6778fb94be93f6c4
aherlei <aherlei564@g.educaand.es>
2024-02-13 14:58:34 +0100

    añadido js y eliminado imagenes que no me hacian falta

---
+++ personal_page_bootstrap/js/app.js
@@ -0,0 +1,13 @@
+const language = document.querySelector(".check")
+
+language.addEventListener('click', lan);
+
+function lan(){
+    let check = language.checked;
+    
+    if (check) {
+        location.href="../index.html";
+    } else {
+        location.href="en/index.html";
+    }
+}
