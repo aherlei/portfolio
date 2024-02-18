7d2ab882b35f98aea3b868a8bb6e8e70cdd07830
aherlei <aherlei564@g.educaand.es>
2024-02-18 22:04:30 +0100

    añadidas las validaciones

--- personal_page_bootstrap/js/app.js
+++ personal_page_bootstrap/js/app.js
@@ -1,3 +1,7 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const emailMsg = document.getElementById('emailHelp');
+const passMsg = document.getElementById('passHelp');
 const language = document.querySelector(".check")
 
 language.addEventListener('click', lan);
@@ -11,3 +15,35 @@ function lan(){
         location.href="es/index.html";
     }
 }
+
+function validation() {
+
+    //!email.value?false:email.style.borderBlockColor = "red";
+    if (email.value == "") {
+        email.style.borderColor = "red";
+        emailMsg.classList.remove("visually-hidden");
+        return false;
+    }
+    
+    if (password.value == ""){
+        password.style.borderColor = "red";
+        passMsg.classList.remove("visually-hidden");
+        return false;
+
+    }
+}
+
+password.addEventListener('keydown', reset, false);
+email.addEventListener('keydown', reset, false);
+
+function reset(){
+
+    emailMsg.classList.add("visually-hidden");
+    email.style.borderColor = "green";;
+    
+    passMsg.classList.add("visually-hidden");
+    password.style.borderColor = "green";;
+}
+
+
+
